81d6aea6679ca70276ceee0083806aae6b072598
Japa-Spk <japinto08@ucatolica.edu.co>
2025-07-07 15:28:34 -0500

    test(add): Add remoteConfig en Tests

--- src/app/services/remote-config.service.spec.ts
+++ src/app/services/remote-config.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-
+import { RemoteConfig } from '@angular/fire/remote-config';
 import { RemoteConfigService } from './remote-config.service';
 
 describe('RemoteConfigService', () => {
   let service: RemoteConfigService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: RemoteConfig, useValue: {} }
+      ]
+    });
     service = TestBed.inject(RemoteConfigService);
   });
 
